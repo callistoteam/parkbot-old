510e7e2b3cce5b75427b076874ae866afbb0139d
Yoruroong <tuntun4132@naver.com>
2020-03-31 19:19:51 +0900

    line

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -6,6 +6,5 @@ module.exports = {
     description: "대기열을 초기화하고 음성채널을 나갑니다.",
     run: async (client, Party, message, embed, youtube) => {
     Party.endStream(message.guild.id, message)
-
     }
 }
