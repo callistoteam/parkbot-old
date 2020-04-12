b8059070da666110a0edbd2999f6d3cef2d96b93
Yoruroong <tuntun4132@naver.com>
2020-04-13 00:10:57 +0900

    fix bug

--- commands/music/play.js
+++ commands/music/play.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js')
 const lang = require('../../lang/kr')
 const { musicloggingChannel } = require("../../config")
 var youtubeThumbnail = require('youtube-thumbnail');
+const yts = require("yt-search")
   
 module.exports = {
     name: "play",
@@ -17,24 +18,19 @@ module.exports = {
     
         }    
         if(!message.data.args) return message.reply('검색 내용을 입력해줘!')
-        await youtube(message.data.args, function(err,r){
-            if(r.videos.length == 0) {
-                embed.addField(lang.commands.play.ns1, lang.commands.play.ns2)
-                return message.channel.send(embed)
-            }
+        yts( message.data.args, function ( err, r ) {
+            if ( err ) throw err
+            console.log(r.videos.length)
+            if(r.videos.length == 0){
+                return message.reply("검색 결과가 없습니다.")
+            } 
             else if(r.videos.length > 1){
-                var txt = ''
-                for(i = 1; i < 6; i++){
-                    if(!r.videos[i-1]) return
-                    txt += `**#${i}** - ${r.videos[i-1].title}\n`
+                Party.addQueue(message.guild.id, r.videos[0].url, message, r.videos[0], youtubeThumbnail('https://youtube.com' + r.videos[0].url))
+		info = r.videos[0].title
+                message.channel.send(`:musical_note: \`${info}\`${hangul.josa(info, '을를')} 재생목록에 대기시켰어! ${Party.getQueue(message.guild.id).length == 1 ? '바로 재생할게!' : '신청곡 앞에 ' + Number(Party.getQueue(message.guild.id).length - 2) + ' 개의 노래가 대기열 대기하고 있어! 좀만 기다려주라구!'}`)
+                if(Party.getGuild(message.guild.id).playing === false) {
+                    Party.startStream(message.guild.id)
                 }
-                    Party.addQueue(message.guild.id, r.videos[0].url, message, r.videos[0], youtubeThumbnail('https://youtube.com' + r.videos[0].url))
-                    info = r.videos[0].title
-                    client.channels.cache.get(musicloggingChannel).send(`\`${r.videos[0].url}\``)
-                    message.channel.send(`\`${info}\`${hangul.josa(info, '을를')} 재생목록에 대기시켰어! ${Party.getQueue(message.guild.id).length == 1 ? '바로 재생할게!' : '신청곡 앞에 ' + Number(Party.getQueue(message.guild.id).length - 2) + ' 개의 노래가 대기열 대기하고 있어! 좀만 기다려주라구!'}`)
-                    if(Party.getGuild(message.guild.id).playing === false) {
-                        Party.startStream(message.guild.id)
-                    }
             }
             else {  
             Party.addQueue(message.guild.id, message.data.args, message, r.videos[0], youtubeThumbnail(message.data.args))
