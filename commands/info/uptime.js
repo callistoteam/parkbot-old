b2a945210ceba457541457da5377df8b20661705
Yoruroong <tuntun4132@naver.com>
2020-04-23 21:19:32 +0900

    space

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -15,6 +15,6 @@ module.exports = {
     aliases: ["uptime", "ㅕㅔ샤ㅡㄷ", "살아있었던시간"],
     description: "봇의 업타임을 확인합니다.",
     run: async (client, Party, message, embed, youtube) => {
-    message.reply(yoruyoru(client))
+        message.reply(yoruyoru(client))
     }
 }
