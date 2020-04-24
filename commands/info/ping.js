c13ad78dc1b2be4d6106efb8ca85ef8911e38c0b
Yoruroong <tuntun4132@naver.com>
2020-04-24 12:21:03 +0900

    space

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,30 +1,30 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "ping",
-  aliases: ["pong", "핑", "ㅔㅑㅜㅎ"],
-  category: "info",
-  description: "봇의 지연시간 측정",
-  usage: "",
-  run: async (client, music, message, embed, youtube, args, lang) => {
-  try{
-    const m = await message.channel.send(lang.commands.ping.fping).then(async msg => {
-      let rpembed = new Discord.MessageEmbed()
-      .setTitle(lang.commands.ping.eping)
-      .setColor("RANDOM")
-      .addField(lang.commands.ping.dtime, `${msg.createdTimestamp - message.createdTimestamp}ms`)
-      .addField(lang.commands.ping.apidtime, `${client.ws.ping}ms`)
-      if(client.ws.ping+(msg.createdTimestamp - message.createdTimestamp) < 537) {
-        rpembed.setFooter(lang.commands.ping.pprg)
-      } else {
-        rpembed.setFooter(lang.commands.ping.pprb)
-      }
-      rpembed.setTimestamp()
-      msg.edit(rpembed)
-    })
-  }catch(e) {
-    message.channel.send("오류");
-    client.users.cache.get('480240821623455746').send(e)
-  }
-  }
+    name: "ping",
+    aliases: ["pong", "핑", "ㅔㅑㅜㅎ"],
+    category: "info",
+    description: "봇의 지연시간 측정",
+    usage: "",
+    run: async (client, music, message, embed, youtube, args, lang) => {
+        try{
+            const m = await message.channel.send(lang.commands.ping.fping).then(async msg => {
+            let rpembed = new Discord.MessageEmbed()
+            .setTitle(lang.commands.ping.eping)
+            .setColor("RANDOM")
+            .addField(lang.commands.ping.dtime, `${msg.createdTimestamp - message.createdTimestamp}ms`)
+            .addField(lang.commands.ping.apidtime, `${client.ws.ping}ms`)
+            if(client.ws.ping+(msg.createdTimestamp - message.createdTimestamp) < 537) {
+                rpembed.setFooter(lang.commands.ping.pprg)
+            } else {
+                rpembed.setFooter(lang.commands.ping.pprb)
+            }
+            rpembed.setTimestamp()
+            msg.edit(rpembed)
+          })
+        }catch(e) {
+            message.channel.send("오류");
+            client.users.cache.get('480240821623455746').send(e)
+        }
+    }
 }
