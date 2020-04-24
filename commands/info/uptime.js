c1db467f34d912027173cb02468e0fcb47ec191c
Yoruroong <tuntun4132@naver.com>
2020-04-24 16:21:43 +0900

    delete and delete

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,13 +1,4 @@
-function yoruyoru(client) {
-    let totalSeconds = (client.uptime / 1000);
-    let days = Math.floor(totalSeconds / 86400);
-    let hours = Math.floor(totalSeconds / 3600) - days*24;
-    totalSeconds %= 3600;
-    let minutes = Math.floor(totalSeconds / 60);
-    let seconds = totalSeconds % 60;
-    let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${Math.round(seconds)} seconds;`
-    return uptime
-}
+const { prefix } = require("../../config")
 
 module.exports = {
     name: "uptime",
@@ -15,6 +6,6 @@ module.exports = {
     aliases: ["uptime", "ㅕㅔ샤ㅡㄷ", "살아있었던시간"],
     description: "봇의 업타임을 확인합니다.",
     run: async (client, Party, message, embed, youtube) => {
-        message.reply(yoruyoru(client))
+        message.reply(`\`${prefix}system\`으로 확인해주세요`)
     }
 }
