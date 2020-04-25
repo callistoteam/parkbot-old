1957f2f29b30db9f46397d68c6b973e9b952d76e
Yoruroong <tuntun4132@naver.com>
2020-04-25 11:56:55 +0900

    pm2

---
+++ commands/fordev/pm2.js
@@ -0,0 +1,18 @@
+﻿const child = require('child_process')
+
+const owners = ["480240821623455746"];
+
+module.exports = {
+    name: "pm2",
+    aliases: ["ㅔㅡ2"],
+    category: "fordev",
+    description: "pm2",
+    usage: "",
+    run: async (client, music, message, embed, youtube, args) => {
+        if (owners.includes(message.author.id) === false) return message.channel.send(lang.commands.pm2.noperm);
+        if(!args[1]) return message.channel.send("name")
+        if(args[0] == "restart"){
+            message.channel.send(`\`\`\`${child.execSync(`pm2 restart ${args[1]}`).toString()}\`\`\``)
+        }
+    }
+}
