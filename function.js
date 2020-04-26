9e2e1d54c589b27370264be5e347a0caa991242c
Yoruroong <tuntun4132@naver.com>
2020-04-26 22:08:03 +0900

    function:yoruyoru

---
+++ function.js
@@ -0,0 +1,10 @@
+function yoruyoru(client) {
+    let totalSeconds = (client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600) - days*24;
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${Math.round(seconds)} seconds;`
+    return uptime
+}
