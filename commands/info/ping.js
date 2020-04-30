ae44f4717901bf8f195ed2be59c04c00771712da
Yoruroong <tuntun4132@naver.com>
2020-04-30 22:01:24 +0900

    fixed bug

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -9,23 +9,24 @@ module.exports = {
     usage: "",
     run: async (client, music, message, embed, youtube, args, lang) => {
         try{
-            const m = await message.channel.send("퐁! 봇의 지연시간을 측정중입니다").then(async msg => {
+            const m = await message.channel.send("<a:loadingforpark:702385005590085632> 잠시만 기다려주세요").then(async msg => {
             let rpembed = new Discord.MessageEmbed()
             .setTitle("퐁!")
             .setColor("RANDOM")
             .addField("지연 시간", `${msg.createdTimestamp - message.createdTimestamp}ms`)
-            .addField("API지연 시간" `${client.ws.ping}ms`)
+            .addField("API지연시간", `${client.ws.ping}ms`)
             if(client.ws.ping+(msg.createdTimestamp - message.createdTimestamp) < 537) {
                 rpembed.setFooter(`${displayname}의 핑 상태 : 정상`)
             } else {
                 rpembed.setFooter(`${displayname}의 핑 상태 : 비정상`)
             }
             rpembed.setTimestamp()
+            msg.edit("Pong")
             msg.edit(rpembed)
           })
         }catch(e) {
             message.channel.send("오류");
-            client.users.cache.get('480240821623455746').send(e)
+            console.log(e)
         }
     }
 }
