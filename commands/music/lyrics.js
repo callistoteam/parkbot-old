4b3909007a2f3507056ce8e9dc88813a08ecb96a
Yoruroong <tuntun4132@naver.com>
2020-05-01 20:21:43 +0900

    umm

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -7,14 +7,20 @@ module.exports = {
     description: "가사를 불러옵니다",
     usage: "<제목>",
     run: async (client, Party, message, embed, youtube) => {
-        try{
+        /*try{
             const m = await message.channel.send("<a:loadingforpark:702385005590085632> 검색중입니다").then(async msg => {
-                var lyrics = await solenolyrics.requestLyricsFor(message.data.args)
+                try{
+                    var lyrics = await solenolyrics.requestLyricsFor(message.data.args)
+                } catch {
+                    return msg.edit(`<@${message.author.id}>, 한국어 검색을 지원하지 않아! 영어로 입력해줘!`)
+                }
                 message.author.send(`\`\`\`${lyrics.substr(0, 1993)}\`\`\``)
-                msg.edit("DM을 확인해주세요")
+                msg.edit(`<@${message.author.id}>, DM을 확인해주세요`)
             })
-        } catch(e) {
+        } catch {
             message.reply("한국어 검색을 지원하지 않아! 영어로 입력해줘!")
         }
+        */
+        message.reply("현재 해당 기능을 이용하실 수 없습니다.")
     }
 }
