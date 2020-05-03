2cd4bdd2d102a888d05b7bd68980322eff009ce3
Yoruroong <tuntun4132@naver.com>
2020-05-03 20:45:17 +0900

    koreanbots

---
+++ commands/fordev/koreanbots.js
@@ -0,0 +1,30 @@
+const owners = ["480240821623455746"];
+const { koreanbotsToken } = require("../../config")
+
+module.exports = {
+    name: "koreanbots",
+    aliases: ["servercountpost"],
+    category: "fordev",
+    description: "post servercount",
+    usage: "",
+    run: async (client, music, message, embed, youtube, args, replaceAll) => {
+        if (owners.includes(message.author.id) === false) return message.channel.send("권한없음")
+        const headers = {
+            "Content-Type": "application/json",
+            "token": koreanbotsToken
+        }
+        
+        const body = { servers: client.guilds.cache.size }
+        
+        try{
+            require("node-fetch")("https://api.koreanbots.cf/bots/servers", {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(body)
+            })
+            message.reply(":ok:")
+        } catch(e) {
+            return message.channel.send(e)
+        }
+    }
+}
