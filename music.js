d9658bc76b16ac84579d4825e4c85f2e1a28453b
Yoruroong <33586417+Yoruroong@users.noreply.github.com>
2020-05-04 11:19:15 +0900

    Update music.js

--- music.js
+++ music.js
@@ -32,7 +32,7 @@ module.exports.next = (identificate) => {
         if (Handles.get(identificate).voiceChannel.members.filter(m => !m.user.bot).size == 0) {
             let embed = new Discord.MessageEmbed()
                 .setColor(require('./config').color)
-                .addField('채널에 아무도 없습니다.', '저런 노래는 듣지도 않으면서 틀어놨군... 전기를 아껴주라구! \`#p\`커맨드로 예전 대기열을 불러오고 재생할 수 있어!',)
+                .addField('채널에 아무도 없습니다.', '저런 노래는 듣지도 않으면서 틀어놨군... 전기를 아껴주라구! \`#p\`커맨드로 대기열을 다시 불러오고 재생할 수 있어!',)
             getGuild(identificate).channel.send(embed)
             Handles.remove(identificate)
         } else if(Handles.get(identificate).voiceChannel.members.filter(m => !m.user.bot).size != 0){
