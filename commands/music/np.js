84cf54c6a34e98b0bf321706d547791ba0222c03
Yoruroong <tuntun4132@naver.com>
2020-05-09 19:28:29 +0900

    fixed np bug

--- commands/music/np.js
+++ commands/music/np.js
@@ -8,10 +8,10 @@ module.exports = {
         embed.addField('재생중인 노래가 없습니다', '이 서버에서 플레이중인 노래는 없어!')
         return message.channel.send(embed)
     } else{
-        return message.author.reply("해당 기능은 이용하실 수 없습니다. 이용에 불편을 드려 죄송합니다")
         let np = Party.getNP(message.guild.id).info
+        console.log(np)
         embed.setTitle(np.title)
-        embed.setDescription(`${sectohhmmss(Math.round(Party.getGuild(message.guild.id).dispatcher.streamingData.count * 0.2 / 10), Party.getNP(message.guild.id).info)} / ${np.timestamp}`)
+        // embed.setDescription(`${sectohhmmss(Math.round(Party.getGuild(message.guild.id).dispatcher.streamingData.count * 0.2 / 10), Party.getNP(message.guild.id).info)} / ${np.timestamp}`)
         embed.addField('게시자', `[${np.author.name}](https://youtube.com${np.author.url})`)
         embed.addField('게시일', np.ago, true)
         embed.addField('조회수', np.views, true)
@@ -20,7 +20,7 @@ module.exports = {
     } 
 }}
 
-function sectohhmmss(yeee, np) {
+/* function sectohhmmss(yeee, np) {
     var sec_num = parseInt(yeee, 10);
     var hours   = Math.floor(sec_num / 3600);
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
@@ -31,4 +31,4 @@ function sectohhmmss(yeee, np) {
     if (seconds < 10) {seconds = "0"+seconds;}
     
     return np.timestamp.match(/:/gi).length == 2 ? hours+ ':' : '' + minutes + ':' + seconds
-}
+} */
