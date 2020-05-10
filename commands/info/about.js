584985f304c62d664ac05af02a52095005250996
Yoruroong <tuntun4132@naver.com>
2020-05-10 09:55:50 +0900

    about

---
+++ commands/info/about.js
@@ -0,0 +1,10 @@
+/* module.exports = {
+    name: "about",
+    category: "info",
+    aliases: ["about"],
+    description: "about",
+    usage: "",
+    run: async (client, Party, message, embed) => {
+        
+    }
+} */
