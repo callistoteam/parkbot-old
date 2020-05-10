73f7283d3ee10d3928743332c83a70464aa1cb05
Yoruroong <tuntun4132@naver.com>
2020-05-10 19:34:09 +0900

    about

--- commands/info/about.js
+++ commands/info/about.js
@@ -1,10 +1,11 @@
-/* module.exports = {
+/*module.exports = {
     name: "about",
     category: "info",
     aliases: ["about"],
     description: "about",
     usage: "",
     run: async (client, Party, message, embed) => {
-        
+        embed.setTitle(require("../../config").displayname)
+        embbed.setDescription("안녕하세요! 파크봇이에요! \n 저는 `2019년 1월 3일 목요일 오전 8시 53분`에 만들어졌어요!")
     }
 } */
