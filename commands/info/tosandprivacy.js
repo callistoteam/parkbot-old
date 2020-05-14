27d2c12a55aa34435516fc1a7af8d4c902c5dc17
Yoruroong <tuntun4132@naver.com>
2020-05-14 23:33:39 +0900

    privacy

---
+++ commands/info/tosandprivacy.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "tosandprivacy",
+    aliases: ["이용약관및개인정보처리방침"],
+    category: "info",
+    description: "이용약관 및 개인정보 처리방침",
+    run: async (client, Party, message, embed) => {
+        embed.setDescription("[개인정보처리방침](https://parkbot.yoru.pe.kr/privacy/)\n[이용약관](https://callisto.team/tos)")
+        message.channel.send(embed)
+    }
+}
