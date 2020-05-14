6d2df8a5fe0e0abff96b0cada1a7f93b5bb8ca21
Yoruroong <tuntun4132@naver.com>
2020-05-14 23:58:05 +0900

    help

--- commands/info/help.js
+++ commands/info/help.js
@@ -20,7 +20,7 @@ module.exports = {
 function getAll(client, message) {
     const embed = new MessageEmbed()
         .setColor("RANDOM")
-        .addField(`${config.displayname} 도움말`, "이 봇을 이용하시면 `#tosandprivary`커맨드로 확인할 수 있는 개인정보 처리 방침 및 이용 약관에 동의하신걸로 간주됩니다.\n봇 초대하기: [여기를 눌러 초대하기](https://parkbot.yoru.pe.kr/)")
+        .addField(`${config.displayname} 도움말`, "이 봇을 이용하시면 `#tosandprivacy`에 동의한걸로 간주됩니다\n봇 초대하기: [여기를 눌러 초대하기](https://parkbot.yoru.pe.kr/)")
         .addField("더 자세한 도움말이 필요하세요?", "[여기를 눌러 확인하기](https://parkbot.yoru.pe.kr/help/)\n`#help [커맨드]`로도 확인하실 수 있습니다.")
 
     const commands = (category) => {
